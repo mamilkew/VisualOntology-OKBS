2efebe32036215589bb1a88015530dd951a26860
mamilkew <mui_milk@hotmail.com>
2018-08-12 01:10:57 +0700

    Added URL of customization Model and Admin-panel customization of Repository, Domain, Property, Forcegraph, Timelinegraph --- Not migrate yet.

--- page/urls.py
+++ page/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from . import views
+from . import views, admin
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('property_datemarked/', admin.DateMarkedAutoComplete.as_view(), name='property_datedmarked'),
+    path('property_faceted/', admin.PropertyAutoComplete.as_view(), name='property_faceted'),
 ]

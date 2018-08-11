126cc7abef8ad68434291a525047e47caef95e89
mamilkew <mui_milk@hotmail.com>
2018-08-12 00:57:52 +0700

    Added Model and Admin-panel customization of Repository, Domain, Property, Forcegraph, Timelinegraph --- Next >>> add URL of customization

--- page/admin.py
+++ page/admin.py
@@ -1,3 +1,100 @@
 from django.contrib import admin
+from dal import autocomplete
+from .models import Repository, Forcegraph, Property, Timelinegraph
+from django import forms
 
 # Register your models here.
+
+
+class RepositoryAdmin(admin.ModelAdmin):
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # This is the case when obj is already created i.e. it's an edit
+            return ['query_path']
+        else:
+            return []
+
+
+# ==========start customizing for Node-link configuration
+class ForcegraphForm(forms.ModelForm):
+    faceted_search = forms.ModelMultipleChoiceField(queryset=Property.objects.all(), required=False,
+                                                    widget=autocomplete.ModelSelect2Multiple(url='property_faceted',
+                                                                                             forward=(
+                                                                                             'repository_query',
+                                                                                             'domain_subject')))
+
+    def __init__(self, *args, **kwargs):
+        super(ForcegraphForm, self).__init__(*args, **kwargs)
+        self.fields['faceted_search'].help_text = 'Please make a blank if you want to get all values of filter.'
+
+    class Meta:
+        model = Forcegraph
+        fields = ('__all__')
+        ordering = ['property_path']
+        extra_kwargs = {'faceted_search': {'allow_blank': True}}
+
+
+class ForcegraphAdmin(admin.ModelAdmin):
+    form = ForcegraphForm
+    list_display = ('page_title', 'created_date', 'updated_date', 'was_published_last')
+    ordering = ['updated_date']
+    list_filter = ['published_date']
+    search_fields = ['page_title', 'domain_subject', 'result']
+# ==========end customizing for Node-link configuration
+
+
+# ==========start customizing for Timeline configuration
+class DateMarkedAutoComplete(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+        if not self.request.user.is_authenticated:
+            return Property.objects.none()
+
+        qs = Property.objects.none()
+
+        domain_subject = self.forwarded.get('domain_subject', None)
+        if domain_subject:
+            qs = Property.objects.filter(domain_prop=domain_subject)
+
+        if self.q:
+            qs = Property.objects.filter(domain_prop=domain_subject).filter(property_path__contains=self.q)
+
+        return qs
+
+
+class TimelinegraphForm(forms.ModelForm):
+    date_marked = forms.ModelChoiceField(queryset=Property.objects.all(), required=False,
+                                         widget=autocomplete.ModelSelect2(url='property_datedmarked',
+                                                                          forward=('repository_query',
+                                                                                             'domain_subject')))
+    faceted_search = forms.ModelMultipleChoiceField(queryset=Property.objects.all(), required=False,
+                                                    widget=autocomplete.ModelSelect2Multiple(url='property_faceted',
+                                                                                             forward=(
+                                                                                             'repository_query',
+                                                                                             'domain_subject')))
+
+    def __init__(self, *args, **kwargs):
+        super(TimelinegraphForm, self).__init__(*args, **kwargs)
+        self.fields['date_marked'].help_text = 'Please select the option which is datetime type.'
+        self.fields['faceted_search'].help_text = 'Please make a blank if you want to get all values of filter.'
+
+    class Meta:
+        model = Timelinegraph
+        fields = ('__all__')
+        ordering = ['property_path']
+        extra_kwargs = {'faceted_search': {'allow_blank': True}}
+
+
+class TimelinegraphAdmin(admin.ModelAdmin):
+    form = TimelinegraphForm
+    list_display = ('page_title', 'created_date', 'updated_date', 'was_published_last', 'date_marked')
+    ordering = ['updated_date']
+    list_filter = ['published_date']
+    search_fields = ['page_title', 'domain_subject', 'result']
+# ==========end customizing for Timeline configuration
+
+
+admin.site.register(Repository, RepositoryAdmin)
+admin.site.register(Forcegraph, ForcegraphAdmin)
+admin.site.register(Timelinegraph, TimelinegraphAdmin)
+admin.site.site_header = 'VisualOntology Adminsitration'    # default: "Django Administration"
+admin.site.index_title = 'Visualization Management'  # default: "Site administration"
+admin.site.site_title = 'VisualOntology site admin'  # default: "Django site admin"
--- page/models.py
+++ page/models.py
@@ -1,3 +1,216 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.utils import timezone
+import json
+from page import sparql_wrapper as spql_wrapper
+from smart_selects.db_fields import ChainedForeignKey, ChainedManyToManyField
+from django.contrib.postgres.fields import JSONField
+
+# from django.dispatch import receiver
+# import datetime
+# from urllib.error import HTTPError
+# from urllib.request import Request, urlopen
+# from urllib.parse import urlencode
+# from base64 import b64encode
+# from django.db.models import Q
 
 # Create your models here.
+
+
+class Repository(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    repo_name = models.CharField(max_length=200)
+    query_path = models.URLField(max_length=500)
+    created_date = models.DateTimeField(default=timezone.now, editable=False)  # cannot edit via admin panel
+    updated_date = models.DateTimeField(blank=True, editable=False)
+
+    class Meta:  # Display in admin panel
+        verbose_name = 'My Repository'
+        verbose_name_plural = 'My Repositories'
+
+    def save(self, *args, **kwargs):  # do something every time you save
+        self.updated_date = timezone.now()
+        super().save(*args, **kwargs)  # Call the "real" save() method.
+
+    def __str__(self):
+        return '%s <%s>' % (self.repo_name, self.query_path)
+
+    # class Meta:
+    #     verbose_name = 'My Repository'
+    #     verbose_name_plural = 'My Repositories'
+
+
+def save_repository(sender, instance, created, **kwargs):
+    if created:
+        sparql_class = 'SELECT DISTINCT ?class ?c_label ' \
+                       + 'WHERE {' \
+                       + '?property rdfs:domain ?class .' \
+                       + 'optional{?class rdfs:label ?c_label}' \
+                       + '}order by ?class'
+        data_class = spql_wrapper.initial_model_api(sparql_class, instance.query_path)
+        results = json.loads(data_class)
+        for result in results['results']['bindings']:
+            Domain.objects.create(domain_path=result.get('class').get('value'), author_id=instance.author_id,
+                                  repository_query=instance)
+
+        sparql_property = 'SELECT DISTINCT ?class ?c_label ?property ?p_label ' \
+                          + 'WHERE {' \
+                          + '?property rdfs:domain ?class .' \
+                          + 'optional{?class rdfs:label ?c_label}' \
+                          + 'optional{?property rdfs:label ?p_label}' \
+                          + '}order by ?class'
+        data_property = spql_wrapper.initial_model_api(sparql_property, instance.query_path)
+        results = json.loads(data_property)
+        for result in results['results']['bindings']:
+            check_class = Domain.objects.filter(repository_query=instance.id).filter(
+                domain_path=result.get('class').get('value')).first()
+            Property.objects.create(property_path=result.get('property').get('value'), author_id=instance.author_id,
+                                    domain_prop=check_class, repository_query=instance)
+
+
+post_save.connect(save_repository, sender=Repository)
+
+
+class Domain(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    domain_path = models.URLField(max_length=500)
+    repository_query = models.ForeignKey(Repository, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now, editable=False)
+
+    def __str__(self):
+        return self.domain_path
+
+
+class Property(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    property_path = models.URLField(max_length=500)
+    domain_prop = models.ForeignKey(Domain, on_delete=models.CASCADE, blank=True, null=True)
+    repository_query = models.ForeignKey(Repository, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now, editable=False)
+
+    def __str__(self):
+        return self.property_path
+
+
+class Forcegraph(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    page_title = models.CharField(max_length=200)
+    repository_query = models.ForeignKey(Repository, on_delete=models.CASCADE)
+    domain_subject = ChainedForeignKey(
+        Domain,
+        chained_field='repository_query',
+        chained_model_field='repository_query',
+        show_all=False,
+        auto_choose=True,
+        sort=True
+    )
+    faceted_search = models.ManyToManyField(Property)
+    # domain_subject = models.ForeignKey(Domain, on_delete=models.CASCADE)
+    source = JSONField(blank=True, null=True, editable=False)
+    result = JSONField(blank=True, null=True, editable=False)
+    created_date = models.DateTimeField(
+        default=timezone.now, editable=False)
+    updated_date = models.DateTimeField(
+        default=timezone.now, editable=False)
+    published_date = models.DateTimeField(
+        blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Graph Node-Link'
+        verbose_name_plural = 'Graph Node-Links'
+
+    def save(self, *args, **kwargs):  # do something every time you save
+        if not self.pk:
+            sparql_all = 'SELECT DISTINCT * WHERE { ?subject rdf:type <' + self.domain_subject.domain_path + '> .' \
+                         + '?subject ?predicate ?object .' \
+                         + 'optional{?subject rdfs:label ?s_label}' \
+                         + 'optional{?predicate rdfs:label ?p_label}' \
+                         + 'optional{?object rdfs:label ?o_label}' \
+                         + 'filter(?object != owl:NamedIndividual)' \
+                         + '}order by ?subject'  # filter(?object != owl:NamedIndividual && ?predicate != rdf:type)
+            data = spql_wrapper.initial_model_api(sparql_all, self.repository_query.query_path)
+            self.source = json.loads(data)
+            self.updated_date = timezone.now()
+            super().save(*args, **kwargs)  # Call the "real" save() method.
+        else:
+            self.updated_date = timezone.now()
+            super().save(*args, **kwargs)  # Call the "real" save() method.
+
+    def was_published_last(self):
+        now = timezone.now()
+        if self.published_date is not None:
+            return self.published_date <= now
+
+    was_published_last.admin_order_field = 'published_date'
+    was_published_last.boolean = True
+    was_published_last.short_description = 'Published ?'
+
+    def __str__(self):
+        return self.page_title
+
+
+class Timelinegraph(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    page_title = models.CharField(max_length=200)
+    repository_query = models.ForeignKey(Repository, on_delete=models.CASCADE)
+    domain_subject = ChainedForeignKey(
+        Domain,
+        chained_field='repository_query',
+        chained_model_field='repository_query',
+        show_all=False,
+        auto_choose=True,
+        sort=True
+    )
+    date_marked = models.ForeignKey(Property, on_delete=models.CASCADE)
+    # date_marked = ChainedForeignKey(
+    #     Property,
+    #     chained_field='domain_subject',
+    #     chained_model_field='domain_prop',
+    #     # related_name='date_marked',
+    #     show_all=False,
+    #     auto_choose=False,
+    #     sort=True
+    # )
+    faceted_search = models.ManyToManyField(Property, related_name='faceted_search')
+    # domain_subject = models.ForeignKey(Domain, on_delete=models.CASCADE)
+    source = JSONField(blank=True, null=True, editable=False)
+    result = JSONField(blank=True, null=True, editable=False)
+    created_date = models.DateTimeField(
+        default=timezone.now, editable=False)
+    updated_date = models.DateTimeField(
+        default=timezone.now, editable=False)
+    published_date = models.DateTimeField(
+        blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Graph Timeline'
+        verbose_name_plural = 'Graph Timelines'
+
+    def save(self, *args, **kwargs):  # do something every time you save
+        if not self.pk:
+            sparql_all = 'SELECT DISTINCT * WHERE { ?subject rdf:type <' + self.domain_subject.domain_path + '> .' \
+                         + '?subject ?predicate ?object .' \
+                         + 'optional{?subject rdfs:label ?s_label}' \
+                         + 'optional{?predicate rdfs:label ?p_label}' \
+                         + 'optional{?object rdfs:label ?o_label}' \
+                         + 'filter(?object != owl:NamedIndividual)' \
+                         + '}order by ?subject'  # filter(?object != owl:NamedIndividual && ?predicate != rdf:type)
+            data = spql_wrapper.initial_model_api(sparql_all, self.repository_query.query_path)
+            self.source = json.loads(data)
+            self.updated_date = timezone.now()
+            super().save(*args, **kwargs)  # Call the "real" save() method.
+        else:
+            self.updated_date = timezone.now()
+            super().save(*args, **kwargs)  # Call the "real" save() method.
+
+    def was_published_last(self):
+        now = timezone.now()
+        if self.published_date is not None:
+            return self.published_date <= now
+
+    was_published_last.admin_order_field = 'published_date'
+    was_published_last.boolean = True
+    was_published_last.short_description = 'Published ?'
+
+    def __str__(self):
+        return self.page_title

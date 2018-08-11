daaa2c4df37b297193f55559721248c74e80e44a
mamilkew <mui_milk@hotmail.com>
2018-08-11 20:02:54 +0700

    Added default Navbar of visualization techniques - Nodelink and Timeline

--- page/context_processors.py
+++ page/context_processors.py
@@ -3,14 +3,14 @@ from django.db.models import Avg, Count, Min, Max, Sum
 
 
 def navbarmain(request):
-    from page.models import Forcegraph
+    from page.models import Forcegraph  # Node-Link Visualization querying the page title to show in Navbar
     raw_query = "SELECT DISTINCT ON (page_title) " \
                 "*, max(published_date) FROM page_forcegraph " \
                 "WHERE published_date is not null " \
                 "GROUP BY page_title, id " \
                 "ORDER BY page_title ASC, published_date DESC"
 
-    from page.models import Timelinegraph
+    from page.models import Timelinegraph  # Timeline Visualization querying the page title to show in Navbar
     timeline_bar = Timelinegraph.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 
     return {'navbarMain': Forcegraph.objects.raw(raw_query), 'navbar_timeline': timeline_bar}

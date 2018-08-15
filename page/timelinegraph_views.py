26fc5bbe32b8554cbe618facbb00dcaa72af53d6
mamilkew <mui_milk@hotmail.com>
2018-08-15 16:51:39 +0700

    Timeline :: added visualization html, js, view and url

---
+++ page/timelinegraph_views.py
@@ -0,0 +1,100 @@
+from django.http import Http404
+from django.shortcuts import render
+from page import extractor_transformation as extractor_trans
+from page import sparql_wrapper as spql_wrapper
+from .models import Timelinegraph
+from django.http import JsonResponse
+import json
+
+
+def timelinegraph(request, page_id):
+    try:
+        project_data = Timelinegraph.objects.get(pk=page_id)
+        date_show = project_data.date_marked.property_path
+        filtering = extractor_trans.faceted_search(project_data.source, project_data.domain_subject.domain_path)
+
+        # results = extractor_trans.transform_api(project_data.source)
+        # new_results = []
+        # new_results.append(extractor_trans.nested_transformation(results, "All", date_show.split('#')[-1]))
+        # project_data.result = new_results
+        # project_data.save()
+
+        # ======== compare facet to display  =========
+        compare_facets = project_data.faceted_search.select_related()
+        if compare_facets:
+            tmps = {}
+            for compare_facet in compare_facets:
+                value = compare_facet.property_path.split('#')[1]
+                if value in filtering['filter_facets']:
+                    tmps[value] = filtering['filter_facets'][value]
+
+            if 'type' in filtering['filter_facets']:
+                tmps['type'] = filtering['filter_facets']['type']
+            if 'comment' in filtering['filter_facets']:
+                tmps['comment'] = filtering['filter_facets']['comment']
+            if 'label' in filtering['filter_facets']:
+                tmps['label'] = filtering['filter_facets']['label']
+
+            filtering['filter_facets'] = tmps
+
+        return render(request, 'page/timelinegraph.html', {'posts': project_data, 'new_results': project_data.result,
+                                                            'filter_facets': filtering['filter_facets'],
+                                                            'filter_prefix': filtering['filter_prefixes'],
+                                                            'dateShow': date_show.split('#')[-1]})
+    except Timelinegraph.DoesNotExist:
+        raise Http404("Post does not exist")
+
+
+def filter_timeline(request):
+    if request.method == 'POST':
+        if request.is_ajax():
+            # prefix_data = request.POST.getlist('prefixes_query')[0]
+            # domain_prefix = json.loads(prefix_data.replace("\'", "\"")).get(request.POST.get('subject_domain'))[0]
+            # domain_prefix_subject = '<' + domain_prefix + '#' + request.POST.get('subject_domain') + '>'
+            domain_prefix_subject = '<' + request.POST.get('subject_domain') + '>'
+            sparql = 'SELECT DISTINCT * WHERE{?subject rdf:type ' + domain_prefix_subject + ' .' \
+                     + '?subject ?predicate ?object . ' \
+                     + 'optional{?subject rdfs:label ?s_label}' \
+                     + 'optional{?predicate rdfs:label ?p_label}' \
+                     + 'optional{?object rdfs:label ?o_label}' \
+                     + 'filter(?object != owl:NamedIndividual)'  # && ?predicate != rdf:type
+            facetdata = []
+            print(request.POST)
+            # print(request.POST.get('subject_domain'))
+            # print(request.POST.keys())
+            for k in request.POST.keys():
+                if k == 'csrfmiddlewaretoken':
+                    pass
+                elif k == 'subject_domain':
+                    pass
+                elif k == 'date_show':
+                    pass
+                elif k == 'link_query':
+                    link_query = request.POST.get('link_query')
+                elif k == 'prefixes_query':
+                    prefix_json = json.loads(request.POST.getlist(k)[0].replace("\'", "\""))
+                else:
+                    if k in prefix_json:
+                        facetdata.extend(request.POST.getlist(k))
+                        print(facetdata)
+                        # print(request.POST.getlist(k))
+                        sparql += spql_wrapper.nested_filter_query(prefix_json.get(k), domain_prefix_subject, k, request.POST.getlist(k))
+            sparql += '}order by ?subject'
+            results = spql_wrapper.call_api(sparql, link_query)
+
+            for (i, item) in enumerate(facetdata):
+                facetdata[i] = item.split('#')[-1]
+
+            new_facetdata = []
+            for result in results:
+                if result.get('object') in facetdata:
+                    if result.get('o_label') is not None:
+                        new_facetdata.append(result.get('o_label'))
+                        # facetdata[facetdata.index(result.get('object'))] = result.get('o_label')
+                    else:
+                        new_facetdata.append(result.get('object'))
+
+            new_results = [extractor_trans.nested_transformation(results, "All", request.POST.get('date_show'))]
+    return JsonResponse({'filter_name': extractor_trans.list_facet(new_facetdata), 'status': sparql, 'query': new_results})
+    # , 'dateShow': request.POST.get('date_show')
+
--- page/urls.py
+++ page/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views, admin, forcegraph_views
+from . import views, admin, forcegraph_views, timelinegraph_views
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -8,4 +8,7 @@ urlpatterns = [
 
     path('forcegraph/<int:page_id>/', forcegraph_views.forcegraph, name='pages_forcegraph'),
     path('filter_forcegraph/', forcegraph_views.filter_forcegraph, name='filter_forcegraph'),  # for ajax in forcegraph.html
+
+    path('timeline/<int:page_id>/', timelinegraph_views.timelinegraph, name='timelinegraph'),
+    path('filter_timeline/', timelinegraph_views.filter_timeline, name='filter_timeline'),
 ]
